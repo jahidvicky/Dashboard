b80e80c9ac95ebde07d9c2163b4773c80f217e5c
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-11-25 16:53:24 +0530

    change url

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -6,8 +6,8 @@ export const CHAT_API_URL = "https://api.ataloptical.org/api";
 // export const SOCKET_URL = "http://localhost:4000";
 export const SOCKET_URL = "https://api.ataloptical.org";
 
-export const IMAGE_URL = "https://api.ataloptical.org/uploads/";
-// export const IMAGE_URL = "http://localhost:4000/uploads/";
+// export const IMAGE_URL = "https://api.ataloptical.org/uploads/";
+export const IMAGE_URL = "http://localhost:4000/uploads/";
 
 const API = axios.create({
   baseURL: CHAT_API_URL,
